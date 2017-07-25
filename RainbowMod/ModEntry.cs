727c9de41ef52d8254de2ac40baac49558474404
githubschman <sarahhubschmail@gmail.com>
2017-07-25 12:29:26 -0400

    deleted monitor logs

--- RainbowMod/ModEntry.cs
+++ RainbowMod/ModEntry.cs
@@ -11,7 +11,6 @@ using System.Text;
 
 namespace TalkedToyou
 {
-	
 	public class ModEntry : Mod
 	{
 		private MouseState MState;
@@ -22,11 +21,7 @@ namespace TalkedToyou
 		public override void Entry(IModHelper helper)
 			{
 				ControlEvents.KeyPressed += this.ControlEvents_KeyPress;
-				ControlEvents.MouseChanged += this.ControlEvents_MouseChanged;
-			}
-			private void ControlEvents_MouseChanged(object sender, EventArgsMouseStateChanged e)
-			{
-			
+				TimeEvents.AfterDayStarted += this.TimeEvents_NewDay;
 			}
 
         private static System.Collections.Generic.Stack<Dialogue> GetCurrentDialogue(NPC character)
@@ -34,17 +29,18 @@ namespace TalkedToyou
             return character.CurrentDialogue;
         }
 		
-		// includes characters you talked to
-		// MOVED THIS TO GLOBAL SCOPE? made it an empty string array.
+		// TalkedToYou in global scope
 		public String[] TalkedToYou = {"null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null"}; 
+
+		public Boolean newDay = false;
+		private void TimeEvents_NewDay(object sender, EventArgs e){
+			newDay = true;
+		}
 
         private void ControlEvents_KeyPress(object sender, EventArgsKeyPressed e)
         {
-
-            if (Context.IsWorldReady && Context.IsPlayerFree && e.KeyPressed.ToString() == "Q")
+            if (Context.IsWorldReady && Context.IsPlayerFree && e.KeyPressed.ToString() == "Q") // when player presses Q
             {
-				
-				
 				var allCharacters = new String[28] {"Alex", "Elliott", "Shane", "Sebastian", "Harvey", "Abigail", "Emily", "Haley", "Leah", "Maru", "Penny", "Caroline", "Clint", "Demetrius", "Evelyn", "George", "Gus", "Jas", "Jodi", "Lewis", "Marnie", "Linus", "Pam", "Pierre", "Robin", "Vincent", "Willy", "Wizard"};
 
 				string currLocation = Game1.currentLocation.ToString();
@@ -60,67 +56,52 @@ namespace TalkedToyou
 				
 				if (Context.IsWorldReady) {
 
-				// should include ALL characters and players children
+				// should include all base game players
 				var NotTalked = new String[28] {"null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null"};
 				
 				// if the person is here, they'll be in this array. resets on every location change.
 				var isHere = new String[28] {"null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null", "null"};
 
-				if(Game1.newDay){
+				while(newDay == true)
+				{
 					int x = 0;
-					while(x >= 27){
-						NotTalked[x] = null;
-						TalkedToYou[x] = null;
-						isHere[x] = null;
+					while(x <= 27){
+						NotTalked[x] = "null";
+						TalkedToYou[x] = "null";
+						isHere[x] = "null";
 						x++;
 					}
+					newDay = false;
 				}
 
-
-				foreach (NPC character in Game1.currentLocation.characters)
+				foreach (NPC character in Game1.currentLocation.characters) // see who is in the area
 					{
-
-						if(character.ToString() == "StardewValley.NPC" && !(character.ToString() == "StardewValley.Characters.Child"))
+						if(character.ToString() == "StardewValley.NPC" && !(character.ToString() == "StardewValley.Characters.Child")) // Only works on NPCs -- no monsters or children!
 						{
-							int talkStack = GetCurrentDialogue(character).Count;
-
-							// add name to isHere array
+							int talkStack = GetCurrentDialogue(character).Count; // get the dialogue stack for each character
 							int j = 0;
 							foreach (String name in isHere){
 								if(isHere[j] == character.name){
 									break;
 								}
-
 								if(isHere[j] == "null"){
 									isHere[j] = character.name;
-									String check = isHere[j];
 									break;
 								}
 								
 								j++;
-							}
-
-							
+							}	
 							// if they have no more dialogue...
 							if(talkStack <= 0){
 								int k = 0;
 								foreach(String name in TalkedToYou) {
-
-
-									this.Monitor.Log($"Going through TalkedToYou!, checking on Not Talked: {NotTalked[k]}");
-
 									// if the person with no dialogue is in the not talked array, take them out
 									if(NotTalked[k] == character.name){
-										this.Monitor.Log($"you talked to {character.name} so we are removing them from the Not Talked array!");
 										NotTalked[k] = "null";
 									}
 									// and put them in the talked to array
 									if(TalkedToYou[k] == "null"){
-											TalkedToYou[k] = character.name;
-											
-											String check2 = TalkedToYou[k];
-											this.Monitor.Log($"You talked to {character.name} today! check: {check2}");
-											// THIS WORKS!
+											TalkedToYou[k] = character.name;					
 											break;
 										}
 										k++;
@@ -136,17 +117,9 @@ namespace TalkedToyou
 						
 						int y = 0;
 						bool talkedTo = false;
-
 						while(y<=26){
-
-							string check = TalkedToYou[y];
-							this.Monitor.Log($"looking to see if {name} is in talked to you, at {check}");
-
-							// if they are in the arrays talkedtoyou, don't include in nottalked
 							if(TalkedToYou[y] == name){
-								this.Monitor.Log($"Yep, you talked to {name}!");
 								talkedTo = true;
-								// THIS WORKS!
 								break;
 							}
 							y++;
@@ -156,20 +129,16 @@ namespace TalkedToyou
 							int z = 0;
 							while(z <= 27){
 								string check2 = NotTalked[z];
-								this.Monitor.Log($"NOT talked to array: {check2}");
 								if(NotTalked[z] == "null"){
-									this.Monitor.Log($"havent talked to {name} yet today, adding them to the not talked to array!");
 									NotTalked[z] = name;
 									break;
 								}
 								z++;
 							}
 						}else if(talkedTo == true){
-							// Talked to whomever, so remove them from notTalked
 							int s = 0;
 							while(s <= 27){
 								if(NotTalked[s] == name){
-									this.Monitor.Log($"Talked to {name} so removing them from Not Talked, lol this is crazy");
 									NotTalked[s] = "null";
 								}
 								s++;
@@ -177,44 +146,39 @@ namespace TalkedToyou
 						}
 					}
 
-						// NotTalked, isHere, & TalkedToYou	
-						String notTalkedToHere = "";
-						String notTalkedNotHere = "";
-						bool show = false;
-						int num1 = 1;
-						int num2 = 1;
+					// NotTalked, isHere, & TalkedToYou	
+					String notTalkedToHere = "";
+					String notTalkedNotHere = "";
+					bool show = false;
+					int num1 = 1;
+					int num2 = 1;
 
-						foreach(String name in isHere){
-							int f = 0;
-							while(f <= 27){
-								if(name == NotTalked[f] && name != "null"){
-									notTalkedToHere += $"{num1}. {name} ";
-									num1++;
-									show = true;
-								}
-								f++;
+					foreach(String name in isHere){
+						int f = 0;
+						while(f <= 27){
+							if(name == NotTalked[f] && name != "null"){
+								notTalkedToHere += $"{num1}. {name} ";
+								num1++;
+								show = true;
 							}
-							
-						}			
-						
-					
-							int g = 0;
-							while(g <= 27){
-								if(NotTalked[g] != "null"){
-									notTalkedNotHere += $"{num2}. {NotTalked[g]} ";
-									num2++;
-								}
-								g++;
-							}
-							
-												
-						
-						
-						if(show == true){
-							Game1.drawObjectDialogue($"Here are the players in {location} who you haven't talked to: {notTalkedToHere}. ---------------------------- You still need to talk to {notTalkedNotHere}");
-						}else{
-							Game1.drawObjectDialogue($"There is no one to talk to in {location}. ---------------------------- You still need to talk to {notTalkedNotHere}");
+							f++;
 						}
+					}			
+						int g = 0;
+						while(g <= 27){
+							if(NotTalked[g] != "null"){
+								notTalkedNotHere += $"{num2}. {NotTalked[g]} ";
+								num2++;
+							}
+							g++;
+						}
+				
+					// finally, draw the messages!
+					if(show == true){
+						Game1.drawLetterMessage($"Here are the players in {location} who you haven't talked to: {notTalkedToHere}. You still need to talk to {notTalkedNotHere}");
+					}else{	
+						Game1.drawLetterMessage($"There is no one to talk to in {location}. You still need to talk to {notTalkedNotHere}");
+					}
 				}
             }
        	 }
